ebe1607c0757d1d1a3f79e200582a2a412006b68
Arthur Levitsky <artsky.task@gmail.com>
2021-05-05 03:33:08 -0400

    Updated temp_v4
    Added images such as Paypal logo and credit cards. main.css has been updated to work with user_order.php page and user_order.html has been deleted (not needed)

--- temp_v4/user_order.php
+++ temp_v4/user_order.php
@@ -10,341 +10,130 @@
     <link rel="stylesheet" href="css/blue_page.css" type="text/css" />
     <link rel="stylesheet" href="css/main.css" type="text/css" />
     <link rel="icon" href="images/sc_logo_CY8_icon.ico" />
-
-    <style>
-      table.TotalTable {
-        width: 50%;
-        border-top: 4px solid #283a89;
-        margin-top: 10px;
-        margin-bottom: 60px;
-        margin-left: auto;
-      }
-
-      th.TableHeader {
-        font-family: "Signika", sans-serif;
-        color: #cbcbcb;
-        font-weight: normal;
-        padding: 5px;
-      }
-
-      #TotalText {
-        color: black;
-        text-align: left;
-        padding: 5px;
-      }
-
-      #TotalQuantityText {
-        color: black;
-        text-align: right;
-        padding: 5px;
-        font-family: "Signika", sans-serif;
-        font-weight: normal;
-      }
-
-      img.CartIcon {
-        display: inline;
-        float: left;
-        margin-left: 10px;
-        margin-top: 10px;
-        margin-bottom: 10px;
-        border: 2px solid black;
-        width: 80px;
-        height: 80px;
-      }
-
-      option {
-        font-family: "Signika", sans-serif;
-        color: black;
-        font-weight: normal;
-      }
-      select {
-        font-family: "Signika", sans-serif;
-        color: black;
-        font-weight: normal;
-        border-radius: 5px;
-        width: 60px;
-        font-size: 12pt;
-      }
-
-      input.OptionSubmit {
-        font-family: "Signika", sans-serif;
-        background-color: #283a89;
-        border-radius: 10px;
-        border: 0;
-        cursor: pointer;
-        color: white;
-        padding: 5px;
-        width: 60px;
-        display: block;
-        margin-right: auto;
-        margin-left: auto;
-        margin-top: 5px;
-      }
-
-      #CartProductText {
-        margin-bottom: 0;
-        color: black;
-        text-align: left;
-        position: relative;
-        left: 10px;
-        font-size: 16pt;
-      }
-
-      #CartPriceText {
-        margin-top: 2px;
-        margin-bottom: 0;
-        color: black;
-        text-align: left;
-        position: relative;
-        left: 10px;
-      }
-
-      a.RemoveItem {
-        font-family: "Signika", sans-serif;
-        color: #cbcbcb;
-        color: #283a89;
-        float: left;
-        position: relative;
-        left: 10px;
-        margin-top: 2px;
-      }
-
-      #SubtotalText {
-        color: black;
-        text-align: right;
-        position: relative;
-        right: 10px;
-        font-size: 14pt;
-      }
-      #state {
-        display: inline-block;
-        color: black;
-        padding: 10px;
-        width: 276px;
-        border: none;
-        border-radius: 50px;
-      }
-
-      #orderTitle {
-        font-family: Orbitron, sans-serif;
-        color: #283a89;
-        text-align: center;
-        margin-top: 30px;
-        margin-left: 50px;
-      }
-
-      #PlaceOrderPanel {
-        position: relative;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 50px;
-        text-align: center;
-        width: 500px;
-        height: 1050px;
-        padding: 20px;
-      }
-
-    </style>
   </head>
   <body>
-    <!-- Create a form that takes a username and password. If the requirements are met through the database, then the user will successfully log in. -->
-    <div class="BoxPanel" id="PlaceOrderPanel" style="width: 600px; height: 1150px">
-      <div id="imageTextCenter">
-        <img
-          class="LogoSmall"
-          id="LogoForm"
-          src="images/sc_logo.png"
-          alt="Swift Care logo"
-        />
-        <h1 class="OrbitronTitle" id="TitleForm">Swift Care - Place Order</h1>
-      </div>
-      
-      <h3 id="orderTitle"> Shipping Address</h3>
+    <div class="BoxPanel" id="LoginPanel" style=" width: 900px; height: 800px">
+    	<div id="imageTextCenter">
+        	<img class="LogoSmall" id="LogoForm" src="images/sc_logo.png" alt="Swift Care logo"/>
+      		<h1 class="OrbitronTitle" id="TitleForm">Swift Care - Place Order</h1>
+      	</div>
+      	<form action="#" method="post" autocomplete="off">
+      		<div style = "display: inline-block; width: 40%">
+      		
+      		<h3 id="orderTitle"> Shipping Address</h3>
+        	<div class="marginContentSpace">
+        		<label class="SignikaLabel" id="LabelForm">Full name</label>
+        		<input class="InputField" id="LoginSignupField" name="fullname" type="name" id="username_sec" required/>
+        	</div>
 
-      <form action="#" method="post" autocomplete="off">
-        <div class="marginContentSpace">
-          <label class="SignikaLabel" id="LabelForm" for="fullname"
-            >Full name</label
-          >
-          <input
-            class="InputField"
-            id="LoginSignupField"
-            name="fullname"
-            type="name"
-            id="username_sec"
-            required
-          />
+        	<div class="marginContentSpace">
+       			<label class="SignikaLabel" id="LabelForm">Street address</label>
+            	<input class="InputField" id="LoginSignupField" name="streetAddress" type="name" required/>
+        	</div>
 
-          <div class="marginContentSpace">
-            <label class="SignikaLabel" id="LabelForm" for="username_sec"
-              >Street address</label
-            >
-            <input
-              class="InputField"
-              id="LoginSignupField"
-              name="streetAddress"
-              type="name"
-              id="username_sec"
-              required
-            />
-          </div>
-            <div class="marginContentSpace">
-              <label class="SignikaLabel" id="LabelForm" for="username_sec"
-                >City</label
-              >
-              <input
-                class="InputField"
-                id="LoginSignupField"
-                name="city"
-                type="name"
-                id="username_sec"
-                required
-              />
-            </div>
+        	<div class="marginContentSpace">
+        		<label class="SignikaLabel" id="LabelForm">City</label>
+            	<input class="InputField" id="LoginSignupField" name="city" type="name" required/>
+        	</div>
 
-              <div class="marginContentSpace">
-                <label class="SignikaLabel" id="LabelForm" for="username_sec"
-                  >State</label
-                >
+			<div class="marginContentSpace">
+                <label class="SignikaLabel" id="LabelForm">State</label>
                 <select name="state" id="state">
-                  <option value="" selected="selected">Select a State</option>
-                  <option value="AL">Alabama</option>
-                  <option value="AK">Alaska</option>
-                  <option value="AZ">Arizona</option>
-                  <option value="AR">Arkansas</option>
-                  <option value="CA">California</option>
-                  <option value="CO">Colorado</option>
-                  <option value="CT">Connecticut</option>
-                  <option value="DE">Delaware</option>
-                  <option value="DC">District Of Columbia</option>
-                  <option value="FL">Florida</option>
-                  <option value="GA">Georgia</option>
-                  <option value="HI">Hawaii</option>
-                  <option value="ID">Idaho</option>
-                  <option value="IL">Illinois</option>
-                  <option value="IN">Indiana</option>
-                  <option value="IA">Iowa</option>
-                  <option value="KS">Kansas</option>
-                  <option value="KY">Kentucky</option>
-                  <option value="LA">Louisiana</option>
-                  <option value="ME">Maine</option>
-                  <option value="MD">Maryland</option>
-                  <option value="MA">Massachusetts</option>
-                  <option value="MI">Michigan</option>
-                  <option value="MN">Minnesota</option>
-                  <option value="MS">Mississippi</option>
-                  <option value="MO">Missouri</option>
-                  <option value="MT">Montana</option>
-                  <option value="NE">Nebraska</option>
-                  <option value="NV">Nevada</option>
-                  <option value="NH">New Hampshire</option>
-                  <option value="NJ">New Jersey</option>
-                  <option value="NM">New Mexico</option>
-                  <option value="NY">New York</option>
-                  <option value="NC">North Carolina</option>
-                  <option value="ND">North Dakota</option>
-                  <option value="OH">Ohio</option>
-                  <option value="OK">Oklahoma</option>
-                  <option value="OR">Oregon</option>
-                  <option value="PA">Pennsylvania</option>
-                  <option value="RI">Rhode Island</option>
-                  <option value="SC">South Carolina</option>
-                  <option value="SD">South Dakota</option>
-                  <option value="TN">Tennessee</option>
-                  <option value="TX">Texas</option>
-                  <option value="UT">Utah</option>
-                  <option value="VT">Vermont</option>
-                  <option value="VA">Virginia</option>
-                  <option value="WA">Washington</option>
-                  <option value="WV">West Virginia</option>
-                  <option value="WI">Wisconsin</option>
-                  <option value="WY">Wyoming</option>
+                	<option value="" selected="selected">Select a State</option>
+                  	<option value="AL">Alabama</option>
+                  	<option value="AK">Alaska</option>
+                  	<option value="AZ">Arizona</option>
+                  	<option value="AR">Arkansas</option>
+                  	<option value="CA">California</option>
+                  	<option value="CO">Colorado</option>
+                  	<option value="CT">Connecticut</option>
+                  	<option value="DE">Delaware</option>
+                  	<option value="DC">District Of Columbia</option>
+                  	<option value="FL">Florida</option>
+                  	<option value="GA">Georgia</option>
+                  	<option value="HI">Hawaii</option>
+                  	<option value="ID">Idaho</option>
+                  	<option value="IL">Illinois</option>
+                  	<option value="IN">Indiana</option>
+                  	<option value="IA">Iowa</option>
+                  	<option value="KS">Kansas</option>
+                  	<option value="KY">Kentucky</option>
+                  	<option value="LA">Louisiana</option>
+                  	<option value="ME">Maine</option>
+                  	<option value="MD">Maryland</option>
+                  	<option value="MA">Massachusetts</option>
+                  	<option value="MI">Michigan</option>
+                  	<option value="MN">Minnesota</option>
+                  	<option value="MS">Mississippi</option>
+                  	<option value="MO">Missouri</option>
+                  	<option value="MT">Montana</option>
+                  	<option value="NE">Nebraska</option>
+                  	<option value="NV">Nevada</option>
+                  	<option value="NH">New Hampshire</option>
+                  	<option value="NJ">New Jersey</option>
+                  	<option value="NM">New Mexico</option>
+                  	<option value="NY">New York</option>
+                  	<option value="NC">North Carolina</option>
+                  	<option value="ND">North Dakota</option>
+                  	<option value="OH">Ohio</option>
+                  	<option value="OK">Oklahoma</option>
+                  	<option value="OR">Oregon</option>
+                  	<option value="PA">Pennsylvania</option>
+                  	<option value="RI">Rhode Island</option>
+                  	<option value="SC">South Carolina</option>
+                  	<option value="SD">South Dakota</option>
+                  	<option value="TN">Tennessee</option>
+                  	<option value="TX">Texas</option>
+	                <option value="UT">Utah</option>
+	                <option value="VT">Vermont</option>
+	                <option value="VA">Virginia</option>
+	                <option value="WA">Washington</option>
+	                <option value="WV">West Virginia</option>
+	                <option value="WI">Wisconsin</option>
+	                <option value="WY">Wyoming</option>
                 </select>
-
-              </div>
-
-              <div class="marginContentSpace">
-                <label class="SignikaLabel" id="LabelForm" for="username_sec"
-                  >Zipcode</label
-                >
-                <input
-                  class="InputField"
-                  id="LoginSignupField"
-                  name="zipcode"
-                  type="text"
-                  id="username_sec"
-                  maxlength="5"
-                  required
-                />
-              </div>
-              
-              <div class="marginContentSpace">
-                <label class="SignikaLabel" id="LabelForm" for="username_sec"
-                  >Email</label
-                >
-                <input
-                  class="InputField"
-                  id="LoginSignupField"
-                  name="streetAddress"
-                  type="email"
-                  id="username_sec"
-                  required
-                />
-              </div>
-
-      <h3 id="orderTitle">Payment Method</h3>
-
-        <div class="marginContentSpace">
-          <label class="SignikaLabel" id="LabelForm" for="username_sec"
-            >Card number</label
-          >
-          <input
-            class="InputField"
-            id="LoginSignupField"
-            name="cardnumber"
-            type="name"
-            id="username_sec"
-            placeholder="Credit/Debit Number"
-            maxlength="19"
-            required
-          />
-        </div>
-
-          <div class="marginContentSpace">
-            <label class="SignikaLabel" id="LabelForm" for="username_sec"
-              >Expiration Date</label
-            >
-            <input
-              class="InputField"
-              id="LoginSignupField"
-              name="cardexpiration"
-              type="month"
-              id="username_sec"
-              required
-            />
-          </div>
-              
-             <div class="marginContentSpace">
-                <label class="SignikaLabel" id="LabelForm" for="username_sec"
-                  >Security</label
-                >
-                <input
-                  class="InputField"
-                  id="LoginSignupField"
-                  name="security"
-                  type="email"
-                  id="username_sec"
-                  placeholder="CVV"
-                  maxlength="3"
-                  required
-                />
-
-              <input class="SubmitButton" type="submit" value="Checkout"/>
             </div>
-      </form> 
-      </div> 
+
+            <div class="marginContentSpace">
+            	<label class="SignikaLabel" id="LabelForm">ZIP Code</label>
+                <input class="InputField" id="LoginSignupField" name="zipcode" type="text" maxlength="5" required/>
+            </div>
+              
+            <div class="marginContentSpace">
+                <label class="SignikaLabel" id="LabelForm">Email</label>
+                <input class="InputField" id="LoginSignupField" name="streetAddress" type="email" required/>
+            </div>
+        	</div>
+        	<div style = "display: inline-block; width: 40%; vertical-align: top">
+      		<h3 id="orderTitle">Payment Method</h3>
+
+        	<div class="marginContentSpace">
+          		<label class="SignikaLabel" id="LabelForm">Card number</label>
+          		<input class="InputField" id="LoginSignupField" name="cardnumber" type="name" placeholder="Credit/Debit Number" maxlength="19" required/>
+        	</div>
+
+          	<div class="marginContentSpace">
+            	<label class="SignikaLabel" id="LabelForm" for="username_sec">Expiration Date</label>
+            	<input class="InputField" id="LoginSignupField" name="cardexpiration" type="month" required/>
+          	</div>
+              
+            <div class="marginContentSpace">
+                <label class="SignikaLabel" id="LabelForm">Security</label>
+                <input class="InputField" id="LoginSignupField" name="security" type="email" placeholder="CVV" maxlength="3" required/>
+            </div>
+            <div class="marginContentSpace">
+              <label class="SignikaLabel" id="LabelForm" style = "position: relative; bottom: 12px">Accepted Cards</label>
+              <img src = "images/credit_cards.png" style = "width: 180px"alt = "Accepted Cards"/>
+            </div>
+        	</div>
+            <input class="SubmitButton" type="submit" value="Place Order"/>   
+      </form>
+      <a href = "https://www.paypal.com/us/signin">
+        <img src = "images/paypal_logo.png" class = "PaypalLogo" alt = "Paypal Logo"/>
+      </a>
+  </div> 
   <!-- Footer portion is used to showcase group number, college, and course. -->
-  <footer style="text-align: center; margin-top: 200px;">
+  <footer style="text-align: center;">
     <p class="SignikaText" id="footerText">
       &copy; 2021 - 2021 Group 2.<br />
       Montclair State University - Computer Science and Technology.<br />
